2d75f7e80868fa00a1e56546b2a61d4ac6c11b22
Saura Naderi <robotsaura@sauras-mbp-2.lan>
2024-06-03 12:51:11 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the model
+model = joblib.load('final_model.pkl')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame(data)
+    predictions = model.predict(df)
+    return jsonify(predictions.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
