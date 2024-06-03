48210e0b34d0f1a1650305c3570eea0e7962df80
Saura Naderi <robotsaura@sauras-mbp-2.lan>
2024-06-03 13:01:12 -0700

    app.py edit commit

--- app.py
+++ app.py
@@ -19,5 +19,6 @@ def predict():
     return jsonify(predictions.tolist())
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
-
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
